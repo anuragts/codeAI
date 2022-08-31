bbe395efadfae0ecb6514650fbb8a509f390fdcf
Anurag <anuragsharma011011@gmail.com>
2022-09-01 02:47:12 +0530

    fetching openai

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>codeAI</title>
+      </Head>
+    <div>
+    Hello World
+    </div>
+    </>
+  )
+}
+
+export default Home
