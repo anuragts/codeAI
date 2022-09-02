c1c053b3e35e58cc3445ffa5c26a9cdd53e47129
Anurag <anuragsharma011011@gmail.com>
2022-09-03 03:38:31 +0530

    js code explain

---
+++ src/pages/api/code/lang.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const { Configuration, OpenAIApi } = require("openai");
+import { config } from "dotenv";
+config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: OPENAI_API_KEY,
+  })
+);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { prompt } = req.body;
+
+  const { data } = await openai.createCompletion({
+    model: "code-davinci-002",
+    prompt:`Javascript${prompt} # \n\n#  Explanation of what the code does  \n\n#`,
+    temperature: 0,
+    max_tokens: 64,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: ["\n\n"],
+  });
+  res.status(200).json(data.choices[0].text);
+};
---
+++ src/pages/lang.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Lang() {
+    const [data , setData] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+
+    const handleSubmit = async (e :any) => {
+        e.preventDefault();
+        const form = e.target;
+        const data = new FormData(form);
+        const dataObj = Object.fromEntries(data);
+        setLoading(true);
+
+        const response = await fetch('/api/code/lang', {
+            method: 'POST',
+            body: JSON.stringify({ prompt: dataObj.prompt }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const result = await response.json();
+        let formatted = result.replace(/(\r\n|\n|\\n|\r)/gm, ` \n ` )
+        let formatted2:any =  formatted.replace(/ +(?= )/g,'');
+
+        setData(formatted2);
+        setLoading(false);
+
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="prompt" placeholder="Enter your query" />
+                <button type="submit">Submit</button>
+            </form>
+            {loading ? <div>Loading...</div> : null}
+            <pre>{data}</pre>
+        </div>
+    )
+}
