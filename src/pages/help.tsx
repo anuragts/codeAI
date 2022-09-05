459d7eb1737b4a69f387a7d9d69fb7c704b2fb20
Anurag <anuragsharma011011@gmail.com>
2022-09-06 00:57:17 +0530

    how to use

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,9 +1,21 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
 
-const Help:NextPage = () => {
+const Help: NextPage = () => {
   return (
-    <div>help</div>
-  )
-}
+    <>
+      <div className="text-center">
+        <div className="my-5 text-3xl "> How to use🤔</div>
+        <div className="flex">
+        <iframe
+        className="mx-auto mt-5 mb-10"
+          width="920"
+          height="450"
+          src="https://www.youtube.com/embed/NwOmuxLQq5o?autoplay=1&mute=1&loop=1"
+        ></iframe>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Help
+export default Help;
