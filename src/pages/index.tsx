76273d00a0481dbae51f9826e08341074f0befc8
Anurag <anuragsharma011011@gmail.com>
2022-09-05 18:13:24 +0530

    frontend tools section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from 'next/router'
-
+import { useRouter } from "next/router";
+import {AiOutlineArrowRight} from "react-icons/ai";
 
 const Home: NextPage = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <>
@@ -17,16 +17,17 @@ const Home: NextPage = () => {
       </div>
       <div>
         <div className="text-xl text-center font-thin text-accent my-4">
-          CodeAI uses openAI codex api to provide <br/> full explanation of code and
-          many more.
+          CodeAI uses openAI codex api to provide <br /> full explanation of
+          code and many more.
         </div>
       </div>
       <div className="text-center">
-        <button className="bg-third text-white rounded px-4 py-2 my-4" onClick={() => router.push('/tools')} >
-          Get Started
+        <button
+          className="bg-third text-xl text-secondary rounded px-10 py-4 my-4 hover:bg-secondary hover:text-third border-2 border-third"
+          onClick={() => router.push("/tools")}>
+          Get Started <AiOutlineArrowRight className="inline text-3xl" />
         </button>
-
-        </div>
+      </div>
     </>
   );
 };
--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import { DiJavascript1 } from "react-icons/di";
+import { SiMysql } from "react-icons/si";
+import {VscFileCode} from 'react-icons/vsc'
 
 function tools() {
+  const router = useRouter();
   return (
-    <div>tools</div>
-  )
+    <>
+      <div className="text-center mt-5 text-5xl">tools</div>
+      <div className="flex text-xl flex-wrap flex-row justify-center">
+        {/* sql */}
+        <div className="w-1/4 h-1/2 border-2 border-third mx-5 my-10">
+          <div className="text-center my-3  text-lg">
+            <SiMysql className="inline text-5xl mx-3 " /> <br />{" "}
+            <span className=""> query generator </span>
+          </div>
+          <div className="text-center my-3">
+            <button
+              className="bg-third text-xl text-secondary rounded px-5 py-2 my-4 hover:bg-secondary hover:text-third border-2 border-third"
+              onClick={() => router.push("/sql")}
+            >
+              Try Now
+            </button>
+          </div>
+        </div>
+        {/* Javascript generator */}
+        <div className="w-1/4 h-1/2 border-2 border-third mx-5 my-10">
+          <div className="text-center my-3 mt-7  text-lg">
+            <DiJavascript1 className="inline text-3xl mx-3 " /> <br />{" "}
+            <span className=""> generator </span>
+          </div>
+          <div className="text-center my-3">
+            <button
+              className="bg-third text-xl text-secondary rounded px-5 py-2 my-4 hover:bg-secondary hover:text-third border-2 border-third"
+              onClick={() => router.push("/js")}
+            >
+              Try Now
+            </button>
+          </div>
+        </div>
+        {/* Lang generator */}
+        <div className="w-1/4 h-1/2 border-2 border-third mx-5 my-10">
+          <div className="text-center my-3 mt-7 text-lg">
+            <VscFileCode className="inline text-3xl mx-3 " /> <br />{" "}
+            <span className=""> generator </span>
+          </div>
+          <div className="text-center my-3">
+            <button
+              className="bg-third text-xl text-secondary rounded px-5 py-2 my-4 hover:bg-secondary hover:text-third border-2 border-third"
+              onClick={() => router.push("/lang")}
+            >
+              Try Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default tools
+export default tools;
