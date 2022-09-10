3490d0cffd40b80d0040f2096e87ca8d2078d560
Anurag <anuragsharma011011@gmail.com>
2022-09-10 13:39:41 +0530

    better guide video

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -5,13 +5,13 @@ const Help: NextPage = () => {
     <>
       <div className="text-center">
         <div className="my-5 md:text-3xl text-2xl"> How to use🤔</div>
-        <div className="flex">
-        <iframe
-        className="mx-auto mt-5 mb-10 "
-          width="920"
-          height="450"
-          src="https://www.youtube.com/embed/NwOmuxLQq5o?autoplay=1&mute=1&loop=1"
-        ></iframe>
+        <div className="flex justify-center my-10">
+            <iframe
+             width="840"
+             height="460"
+              src="https://www.loom.com/embed/5e635fb1a95544d88c9005c1d8ebc54e"
+              className="text-center"
+            ></iframe>
         </div>
       </div>
     </>
