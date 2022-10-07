d997ddbebfded009e9a0053125cfc626d0d033a5
Anurag <anuragsharma011011@gmail.com>
2022-10-07 13:27:47 +0530

    fix translate bug

--- src/pages/api/code/translate.ts
+++ src/pages/api/code/translate.ts
@@ -1,27 +1,28 @@
-import { NextApiRequest,NextApiResponse } from "next";
-import {openAi} from "../../../config/openAi.config";
+import { NextApiRequest, NextApiResponse } from "next";
+import { openAi } from "../../../config/openAi.config";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { prompt } = req.body
-    const {lang1} = req.body
-    const {lang2} = req.body
-const data = await openAi.createCompletion({
-  model: "code-davinci-002",
-  prompt: `##### Translate this function  from ${lang1} into ${lang2}\n### ${lang1}\n ${prompt}    \n    ### ${lang2}`,
-  temperature: 0,
-  max_tokens: 150,
-  top_p: 1.0,
-  frequency_penalty: 0.0,
-  presence_penalty: 0.0,
-  stop: ["###"],
-});
-if (data) {
-  res.status(200).json(data);
-} else {
-  res.status(500).json({ error: "No data fetched" });
-}    
-}
+  const { prompt } = req.body;
+  const { lang1 } = req.body;
+  const { lang2 } = req.body;
+  const response = await openAi.createCompletion({
+    model: "code-davinci-002",
+    prompt: `##### Translate this function  from ${lang1} into ${lang2}\n### ${lang1}\n ${prompt}    \n    ### ${lang2}`,
+    temperature: 0,
+    max_tokens: 150,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["###"],
+  });
+
+  if (response.data) {
+    res.status(200).json(response.data);
+  } else {
+    res.status(500).json({ error: "No data fetched" });
+  }
+};
+
 export const Config = {
-    runtime: 'experimental-edge',
-  };
-  
+  runtime: "experimental-edge",
+};
